0b402218051bf0466ca70a42a1489a3df50b125b
jungtop <jungtop@jungtops-MacBook-Pro.local>
2024-02-15 15:55:43 -0500

    added homeboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./components/navbar/Navbar";
 import Services from "./components/services/Services";
+import Homeboard from "./components/homeboard/Homeboard";
+import styles from "./App.module.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.maincontainer}>
+      <div className={styles.homecontainer}>
       <Navbar />
+      <Homeboard />
+      </div>
       <Services />
     </div>
   );
--- src/components/homeboard/Homeboard.jsx
+++ src/components/homeboard/Homeboard.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+// Homeboard.js
+import React from 'react';
+import styles from './Homeboard.module.css';
 
 function Homeboard() {
   return (
-    <div>
-      
+    <div className={styles.box}>
+      <div className={styles.overlay}>
+        <div className={styles.content}>
+          <div className={styles.heading}>Aria Plumbing in GTA</div>
+          <div className={styles.description}>Your family Plumber</div>
+          <div className={styles.buttons}>
+            <a href="/" className={styles.button}>Get Quote</a>
+            <a href="/" className={styles.button}>Call Us</a>
+          </div>
+        </div>
+      </div>
+      {/* Image is set as a background in CSS */}
     </div>
-  )
+  );
 }
 
-export default Homeboard
+export default Homeboard;
