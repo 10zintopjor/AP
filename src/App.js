333f29f1bcb5700ac4dfa99a16f7c92b27e3e03e
jungtop <jungtop@jungtops-MacBook-Pro.local>
2024-03-19 14:41:42 -0400

    cahtbot ui added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import Navbar from "./components/navbar/Navbar";
-import Services from "./components/services/Services";
-import Homeboard from "./components/homeboard/Homeboard";
-import Footer from "./components/footer/Footer";
-import CarouselReviews from "./components/review/CarouselReviews";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import ExploreSolutions from "./components/services/ExploreService";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div >
-      <Navbar />
-      <Homeboard />
-      <ExploreSolutions />
-      <Services />
-      <CarouselReviews />
-      <Footer />
-    </div>
-  );
+  return <>
+  <Home />
+  </>
 }
 
 export default App;
